81f9631046196b6c82267842ef83909ad131c045
okadakazuyuki <okadathethird@gmail.com>
2022-09-05 18:43:14 +0900

    event.js修正01

--- static/js/event.js
+++ static/js/event.js
@@ -4,6 +4,8 @@ $(function() {
  var html = "";
  var article_count = 0;
  var html_array = [];
+ var eid_list = [];
+ var html_list = {};
  $.ajax({
    "type" : "GET",
    "url": "https://www.sekisuihouse.co.jp/event/cgi/api/eventlist.php?siten=364003&hits=100&entrydate=1",
@@ -11,12 +13,18 @@ $(function() {
    "success" : function(data){console.log(data);
      for(i in data){
        if(i > 99) break;
+       eid_list.push(data[i].event_id) ;
+       html_list[data[i].event_id] = "";
+       console.log(eid_list);
        $.ajax({
          "type" : "GET",
          "url": "https://www.sekisuihouse.co.jp/event/cgi/api/eventdetail.php?id="+data[i].event_id,
          "dataType" : 'jsonp',
          "success" : function(d_data){
            var _img = d_data.event.campaign.img !=  "" ? d_data.event.campaign.img : d_data.event.image.img;
+           if(_img === ""){
+            _img = d_data.event.kaijo.img;
+           }
            
            html = '        <table class="eventTables">\
           <tbody>\
@@ -36,18 +44,21 @@ $(function() {
           </tbody>\
         </table>';
            article_count++;
+           html_list[d_data.event.event_id] = html;
 
-           var num = new Date(d_data.event.kikan.d_s.slice(0,-3)).getTime();// * 1;
-           html_array.push([{"date":num,"html":html}]);
+           /*var num = new Date(d_data.event.kikan.d_s.slice(0,-3)).getTime();// * 1;
+           html_array.push([{"date":num,"html":html}]);*/
            if(article_count == 100 || data.length == article_count){
-             html_array.sort(function(a,b){
-               
+             /*html_array.sort(function(a,b){
                if(a[0].date > b[0].date) return -1;
                if(a[0].date < b[0].date) return 1;
                return 0;
              });
              for(v in html_array){
                $(".sections.event .inner").append(html_array[v][0]["html"]);
+             }*/
+             for(h in eid_list){
+              $(".sections.event .inner").append(html_list[eid_list[h]]);
              }
            }
 
